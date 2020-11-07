aa3dde1e700189d3611e15d868b38df8201dbd26
KineticTactic <rudrajeetpal64@gmail.com>
2020-11-07 14:10:26 +0530

    Added typingtest command

---
+++ commands/fun/typingtest.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "typingtest",
+    description: "Typing test!",
+    args: false,
+    category: "utility",
+    execute(message, args) {
+        const text = "The quick brown fox jumps over the lazy dog";
+        let startTime;
+        message.channel
+            .send(`Type this as fast as you can: \n \`${text}\``)
+            .then((message) => (startTime = Date.now()));
+
+        const collector = new Discord.MessageCollector(
+            message.channel,
+            (m) => m.author.id === message.author.id,
+            { time: 30000 }
+        );
+
+        collector.on("collect", (message) => {
+            if (message.content.toLowerCase() === text.toLowerCase()) {
+                const timeTaken = (Date.now() - startTime) / 1000;
+                const wpm =
+                    Math.round(
+                        (text.length / 5 / (timeTaken / 60) + Number.EPSILON) *
+                            100
+                    ) / 100;
+                message.channel.send(
+                    `Well Done! It took you ${timeTaken} seconds. Your typing speed is ${wpm} WPM.`
+                );
+            } else {
+                message.channel.send("Oops! You made a typo.");
+            }
+            collector.stop();
+        });
+    },
+};
